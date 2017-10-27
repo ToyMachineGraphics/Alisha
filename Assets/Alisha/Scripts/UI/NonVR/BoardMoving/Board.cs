e90dc7615aabad0fb56f0c0ff65c4eba01865d88
cyshenfrog <cyshenfrog@gmail.com>
2017-10-27 08:12:55 +0800

    Creat NonVR Scene, Board Script

---
+++ Assets/Alisha/Scripts/UI/NonVR/BoardMoving/Board.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public Transform AnchorPoint;
+
+    private float _width;
+    private float _height;
+
+    // Use this for initialization
+    private void Start()
+    {
+        _width = ((RectTransform)transform).sizeDelta.x;
+        _height = ((RectTransform)transform).sizeDelta.y;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
