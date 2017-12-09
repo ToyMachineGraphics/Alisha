84b2603077b6c9c6fe62b0b34cefa42bd9ea865b
CL Wu <hardcodefool@gmail.com>
2017-12-09 12:38:16 +0800

    * Drag NonVRPlayer(Robot) to ALishaNetworkManager.
    Basic Aisha script.

---
+++ Assets/Alisha/Components/Aisha/Aisha.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Aisha : NetworkBehaviour
+{
+    public VRController Controller;
+
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+
+        Controller = VRController.Instance;
+    }
+
+    private void Start ()
+    {
+    }
+	
+	private void Update ()
+    {
+		
+	}
+}
